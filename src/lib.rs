706490f5b5e73449a36076d80c57f1abed310462
Zekinler <zekinler7@gmail.com>
2024-10-05 18:09:42 -0400

    remove explicit type definition

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ const MAXIMUM_PATTERN_LENGTH: usize = 64;
 const MINIMUM_PLAYER_COUNT: usize = 1;
 const MAXIMUM_PLAYER_COUNT: usize = 8;
 
-const CHARACTERS: [char; MAXIMUM_PATTERN_LENGTH] = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','-','_'];
+const CHARACTERS := ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','-','_'];
 
 enum InitialGuessError {
     WrongLength,
